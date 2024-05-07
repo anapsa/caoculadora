db1e3636c6fc0d7212594de22f59e08a5119cc5c
Ana Paula Sá <anapaulasapaiva@gmail.com>
2024-05-07 17:38:38 -0300

    atualiza a interface do app: adiciona textfields na vstack

--- caoculadora/caoculadora/ContentView.swift
+++ caoculadora/caoculadora/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var years: Int? = nil;
+    @State var months: Int? = nil
     var body: some View {
-        VStack {
-            Image(systemName: "pawprint")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (alignment: .leading){
+            Text("Qual a idade do seu cão?")
+            Text("Anos")
+            TextField("Digite a idade do seu cão tem", value: $years, format: .number)
+            Text("Meses")
+            TextField("Digite quantos meses seu cão tem", value: $months, format: .number)
         }
         .padding()
+        .textFieldStyle(.roundedBorder)
+        .keyboardType(.numberPad)
+        .bold()
+        .fontDesign(.rounded)
+        
     }
 }
 
