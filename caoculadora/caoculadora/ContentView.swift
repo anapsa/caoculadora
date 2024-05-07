3b6064e86bd3c0d9359cf8c0d82283554cbc1462
Ana Paula Sá <anapaulasapaiva@gmail.com>
2024-05-07 16:12:31 -0300

    adiciona primeiro arquivo de swiftUI

---
+++ caoculadora/caoculadora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  caoculadora
+//
+//  Created by Ana Paula Sá Barreto Paiva da Cunha on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "pawprint")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ caoculadora/caoculadora/caoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  caoculadoraApp.swift
+//  caoculadora
+//
+//  Created by Ana Paula Sá Barreto Paiva da Cunha on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct caoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
