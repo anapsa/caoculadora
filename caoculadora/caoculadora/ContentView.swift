c5bb0bc0ef51194506f8641f6a4d86417899876e
Ana Paula Sá <anapaulasapaiva@gmail.com>
2024-05-08 18:00:56 -0300

    adiciona botao e asset

--- caoculadora/caoculadora/ContentView.swift
+++ caoculadora/caoculadora/ContentView.swift
@@ -8,15 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var years: Int? = nil;
+    @State var years: Int? = nil
     @State var months: Int? = nil
+    @State var result: Int? = nil
+    
     var body: some View {
+        
         VStack (alignment: .leading){
             Text("Qual a idade do seu cão?")
             Text("Anos")
             TextField("Digite a idade do seu cão tem", value: $years, format: .number)
             Text("Meses")
             TextField("Digite quantos meses seu cão tem", value: $months, format: .number)
+            Text("Porte")
+            if let result {
+                /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+            } else {
+                Image(ImageResource.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 150)
+                    .frame(maxWidth: .infinity)
+                .shadow(radius: 20)
+            }
+            Button(action: {
+                result = 23
+            },
+                   label: {
+                    ZStack{
+                        Color.yellow
+                        Text("Cãocular")
+                            .foregroundStyle(.white)
+                    
+                    }
+                    .cornerRadius(10)
+                }
+            )
+            .frame(maxHeight: 50)
+            
         }
         .padding()
         .textFieldStyle(.roundedBorder)
