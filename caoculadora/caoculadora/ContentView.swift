8fb2b0b09dd987d2994f268f80f64dc58993bc89
Ana Paula Sá <anapaulasapaiva@gmail.com>
2024-05-09 17:49:04 -0300

    implementa segmented control e calculo da idade do cachorro

--- caoculadora/caoculadora/ContentView.swift
+++ caoculadora/caoculadora/ContentView.swift
@@ -11,18 +11,30 @@ struct ContentView: View {
     @State var years: Int? = nil
     @State var months: Int? = nil
     @State var result: Int? = nil
-    
+    @State var porte: String = "pequeno"
+    let portes = ["pequeno", "médio", "grande"]
     var body: some View {
-        
         VStack (alignment: .leading){
+       
             Text("Qual a idade do seu cão?")
             Text("Anos")
             TextField("Digite a idade do seu cão tem", value: $years, format: .number)
             Text("Meses")
             TextField("Digite quantos meses seu cão tem", value: $months, format: .number)
             Text("Porte")
+            Picker("Porte", selection: $porte){
+                ForEach(portes, id: \.self) { porte in
+                    Text(porte)
+                        .tag(porte)
+                }
+            }
+            .pickerStyle(.segmented)
+            Divider()
+            Spacer()
             if let result {
-                /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+                Text("A idade do seu cachorro em anos humanos é")
+                Text("\(result) anos")
+              
             } else {
                 Image(ImageResource.image)
                     .resizable()
@@ -31,9 +43,8 @@ struct ContentView: View {
                     .frame(maxWidth: .infinity)
                 .shadow(radius: 20)
             }
-            Button(action: {
-                result = 23
-            },
+            Spacer()
+            Button(action: processYears,
                    label: {
                     ZStack{
                         Color.yellow
@@ -45,14 +56,22 @@ struct ContentView: View {
                 }
             )
             .frame(maxHeight: 50)
-            
         }
         .padding()
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
         .bold()
         .fontDesign(.rounded)
-        
+
+    }
+    func processYears(){
+        guard 
+            let years, let months
+        else {
+            print("preencha o campo de entrada")
+            return
+        }
+        result = years*7 + (months*7/12)
     }
 }
 
